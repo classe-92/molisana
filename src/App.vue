2cdc1a045ecb74fc295f0863a3a8b7b6d2a96ff5
klelia <clelia.fradella@gmail.com>
2023-04-13 11:04:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>Ciao</h1>
+    <img src="/img/la-molisana-logo.png" alt="Logo la Molisana">
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: green
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Pippo</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+}
+</style>
