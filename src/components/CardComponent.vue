df43cadf555b98981e02192c2813b1cb1f1da2e9
klelia <clelia.fradella@gmail.com>
2023-04-14 12:22:53 +0200

    esempi $emit

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 gy-3">
         <div class="card">
             <div class="card-body">
                 <div class="card-image">
@@ -8,6 +8,7 @@
                 <div class="card-title">
                     {{ title }}
                 </div>
+                <button @click='prova'>Clicca</button>
             </div>
         </div>
     </div>
@@ -16,7 +17,12 @@
 <script>
 export default {
     name: 'CardComponent',
-    props: ['image', 'title']
+    props: ['image', 'title'],
+    methods: {
+        prova() {
+            this.$emit('cambiatitolo', 'ciao');
+        }
+    }
 }
 </script>
 
